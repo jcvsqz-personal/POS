8062b76ee10c94c8134b05f92d0505af67a24065
jcvsqz-personal <rak29jccc@gmail.com>
2023-01-16 16:08:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdPbFkDuN1aWDeBflf5PqkSN0WSE0TKKA",
+    authDomain: "clips-c6a4f.firebaseapp.com",
+    projectId: "clips-c6a4f",
+    storageBucket: "clips-c6a4f.appspot.com",
+    appId: "1:716507575532:web:0e90d9412ce4f7211221c9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
